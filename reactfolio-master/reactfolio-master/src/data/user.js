c18270bb881ebebb3d1b574b980a9bcaded91e12
Riri <53974112+Rih348@users.noreply.github.com>
2023-09-28 02:58:39 +0300

    Update user.js

--- reactfolio-master/reactfolio-master/src/data/user.js
+++ reactfolio-master/reactfolio-master/src/data/user.js
@@ -13,7 +13,7 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Cloud Engineer enthusiast, Full-stack web developer.",
+		title: "Cloud enthusiast, Full-stack web developer.",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
